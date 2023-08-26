b82bb981cb5f6323d9687b9e671cb99b0471e9f9
Arul Rosario <itsarrowhere380@gmail.com>
2023-08-26 15:02:34 +0530

    User Page Mock Up Done!

---
+++ lib/Widgets/Account/Account.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:login_screen/Widgets/Home/Home.dart';
+
+class Account extends StatelessWidget {
+  const Account({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+  _onItemTapped(int num) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Home(),
+            ));
+    }
+
+    return Scaffold(
+      body: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              // margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Column(
+                children: [
+                  Container(
+                    child: Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(fit: BoxFit.cover, image: NetworkImage("https://i0.wp.com/blog.photoshelter.com/wp-content/uploads/2023/04/national-photo-month-blog-cover.jpg?resize=350%2C200&ssl=1")
+                            )
+                          ), 
+                        ), Container(
+                          height: 250,
+                          alignment: Alignment.bottomLeft,
+                          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                          child: Text("We Are V Studio", style: GoogleFonts.poppins(color: Colors.white, fontSize: 22),),
+                        )
+                      ]
+                    )
+                  ),
+
+                  Container(
+                    
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                      color: Colors.white,
+                       boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(169, 193, 193, 193),
+                          blurRadius: 30,
+                          spreadRadius: 4,
+                        ),
+                        
+                      ]
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 17, horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text( "Personal Informarion", style: GoogleFonts.poppins(fontSize: 17, fontWeight: FontWeight.bold),),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 8)),
+                        Row(
+                          children: [
+                            Container(
+                              child: Text("Name: ", style: GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w600),),
+                            ), Container(
+                              child: Text("Ramesh", style: GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w400),),
+                            )
+                          ],
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 3),),
+                        Row(
+                          children: [
+                            Container(
+                              child: Text("Phone Number: ", style: GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w600),),
+                            ), Container(
+                              child: Text("9804298149", style: GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w400),),
+                            )
+                          ],
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 3),),
+                        Row(
+                          children: [
+                            Container(
+                              child: Text("E-mail: ", style: GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w600),),
+                            ), Container(
+                              child: Text("itsarrowhere380@gmail.com", style: GoogleFonts.poppins(fontSize: 13, fontWeight: FontWeight.w400),),
+                            )
+                          ],
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 3),)
+                      ],
+                    ),
+                  ),
+
+                  Padding(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20)),
+
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                      color: Colors.white,
+                       boxShadow: [
+                        BoxShadow(
+                          color: Color.fromARGB(169, 193, 193, 193),
+                          blurRadius: 30,
+                          spreadRadius: 4,
+                        ),
+                        
+                      ]
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        Text("Your Orders", style: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500),),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 6, horizontal: 20)),
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                              ),
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    height: 130,
+                                    width: 220,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(Radius.circular(3)),
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage("https://www.babsboardwellweddings.co.uk/wp-content/uploads/2020/06/Post-Wedding-Photographer-16.jpg"))
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 130,
+                                    width: 180,
+                                    alignment: Alignment.bottomLeft,
+                                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                                    child: Text("Marriage Shoot", style: GoogleFonts.poppins(textStyle: TextStyle(color: Colors.white)),),
+                                  )
+                                ],
+                              ),
+                            )
+                          ]
+                        )
+                      ],
+                    )
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text("Contect Us", style: GoogleFonts.poppins(),)
+                      ],
+                    ),
+                  )
+                ]
+              )
+            )
+          ],
+        ),
+        bottomSheet: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 30,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+              size: 30,
+            ),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: 1,
+        selectedItemColor: Color.fromARGB(255, 255, 0, 0),
+        onTap: _onItemTapped,
+      ),);
+  }
+}
--- lib/Widgets/Home/Home.dart
+++ lib/Widgets/Home/Home.dart
@@ -8,6 +8,7 @@ import 'package:login_screen/Widgets/Home/Slider/SliderHome.dart';
 import 'package:login_screen/Widgets/Home/SlotContainer/SlotContainer.dart';
 import 'package:login_screen/loaderLine/loaderLines.dart';
 import 'package:http/http.dart' as http;
+import 'package:login_screen/Widgets/Account/Account.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -66,106 +67,120 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     _onItemTapped(int num) {
-      setState(() {
-        Number = num;
-      });
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Account(),
+          ));
     }
 
-    return Scaffold(
-      extendBodyBehindAppBar: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        // Can include Flexible Space
-        backgroundColor: Color.fromRGBO(255, 255, 255, 1),
-        centerTitle: true,
-        title: Text("V Studios", style: GoogleFonts.poppins(textStyle: TextStyle(fontSize: 19, color:  Colors.black87, fontWeight: FontWeight.w500)),),
-        toolbarHeight: 67.0,
-      ),
-      body: loaderScreen
-          ? SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SliderHome(),
-                  Padding(padding: EdgeInsets.symmetric(vertical: 13)),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+    return Number == 0
+        ? Scaffold(
+            extendBodyBehindAppBar: false,
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              // Can include Flexible Space
+              backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+              centerTitle: true,
+              title: Text(
+                "V Studios",
+                style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                        fontSize: 19,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w500)),
+              ),
+              toolbarHeight: 67.0,
+            ),
+            body: loaderScreen
+                ? SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Flexible(
-                          flex: 1,
-                          child: Divider(
-                            thickness: 0.2,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                          ),
-                        ),
-                        Flexible(
-                          flex: 1,
-                          child: Container(
-                            padding: EdgeInsets.symmetric(horizontal: 17),
-                            child: Text(
-                              "FEATURED",
-                              textAlign: TextAlign.left,
-                              style: GoogleFonts.quattrocento(
-                                textStyle: TextStyle(
-                                    color: Color.fromARGB(255, 148, 148, 148),
-                                    fontWeight: FontWeight.w100,
-                                    fontSize: 12),
+                        SliderHome(),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 13)),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Flexible(
+                                flex: 1,
+                                child: Divider(
+                                  thickness: 0.2,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
                               ),
-                            ),
+                              Flexible(
+                                flex: 1,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 17),
+                                  child: Text(
+                                    "FEATURED",
+                                    textAlign: TextAlign.left,
+                                    style: GoogleFonts.quattrocento(
+                                      textStyle: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 148, 148, 148),
+                                          fontWeight: FontWeight.w100,
+                                          fontSize: 12),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Flexible(
+                                flex: 1,
+                                child: Divider(
+                                  thickness: 0.2,
+                                  color: Color.fromARGB(255, 52, 52, 52),
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                        Flexible(
-                          flex: 1,
-                          child: Divider(
-                            thickness: 0.2,
-                            color: Color.fromARGB(255, 52, 52, 52),
-                          ),
-                        )
+                        Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+                        for (var i = 0; i < jsonData.length; i++)
+                          SlotContainer(
+                              shootName: jsonData[i]["name"],
+                              description: jsonData[i]["description"],
+                              image: jsonData[i]["image"],
+                              snap: jsonData[i]),
                       ],
                     ),
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      LoaderScreen(loaderString: loaderString),
+                    ],
                   ),
-                  Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-                  for (var i = 0; i < jsonData.length; i++)
-                    SlotContainer(
-                        shootName: jsonData[i]["name"],
-                        description: jsonData[i]["description"],
-                        image: jsonData[i]["image"],
-                        snap: jsonData[i]),
-                ],
-              ),
-            )
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                LoaderScreen(loaderString: loaderString),
-              ],
-            ),
-      bottomNavigationBar: loaderScreen? BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              size: 30,
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_circle,
-              size: 30,
-            ),
-            label: 'Account',
-          ),
-        ],
-        currentIndex: Number,
-        selectedItemColor: Color.fromARGB(255, 255, 0, 0),
-        onTap: _onItemTapped,
-      ): null,
-    );
+            bottomNavigationBar: loaderScreen
+                ? BottomNavigationBar(
+                    showSelectedLabels: false,
+                    showUnselectedLabels: false,
+                    items: const <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.home,
+                          size: 30,
+                        ),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(
+                          Icons.account_circle,
+                          size: 30,
+                        ),
+                        label: 'Account',
+                      ),
+                    ],
+                    currentIndex: Number,
+                    selectedItemColor: Color.fromARGB(255, 255, 0, 0),
+                    onTap: _onItemTapped,
+                  )
+                : null,
+          )
+        : Account();
   }
 }
